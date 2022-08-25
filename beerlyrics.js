2c9ae5ea7d9eb1d797c7e6ff31122d8e3c81de1f
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-08-25 15:21:28 +0100

    fanta lyrics

---
+++ beerlyrics.js
@@ -0,0 +1,5 @@
+for (var bottles =99; bottles > 0 ; bottles--) {
+    if(bottles >0){
+    lyrics.innerHTML += "⁕ " + bottles + " bottles of fanta on the wall " + bottles + " bottles of fanta. <br> Take one down and pass it around " + (bottles-1) + " bottles of fanta on the wall." + "<br>" + "<br>" + "<br>"
+    } 
+}
